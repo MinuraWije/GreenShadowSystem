8c0e1bb8d491de304b17c087efbacc2ab9ed043c
MinuraWije <minurawije@gmail.com>
2024-11-11 12:04:58 +0530

    crop entity done

---
+++ src/main/java/org/example/greenshadowsystem/entity/impl/CropEntity.java
@@ -0,0 +1,26 @@
+package org.example.greenshadowsystem.entity.impl;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.greenshadowsystem.entity.SuperEntity;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "crop")
+public class CropEntity implements SuperEntity {
+    @Id
+    private String cropCode;
+    private String cropName;
+    private String cropScientificName;
+    private String category;
+    @Column(columnDefinition = "LONGTEXT")
+    private String img;
+    private String season;
+    @ManyToOne
+    @JoinColumn(name = "fieldCode", nullable = false)
+    private FieldEntity field;
+}
