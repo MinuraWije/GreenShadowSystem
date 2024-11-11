73ddd161d5f3c68b44cbf4ca4e1dd36b55c14e06
MinuraWije <minurawije@gmail.com>
2024-11-11 10:36:58 +0530

    Initial commit

---
+++ src/main/java/org/example/greenshadowsystem/GreenShadowSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.greenshadowsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShadowSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenShadowSystemApplication.class, args);
+    }
+
+}
