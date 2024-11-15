673b9023cb0a7e1fb51f2cdbe3c902c852514d03
MinuraWije <minurawije@gmail.com>
2024-11-15 10:33:47 +0530

    created associate entity for Field and staff

---
+++ src/main/java/org/example/greenshadowsystem/entity/impl/FieldedStaff.java
@@ -0,0 +1,28 @@
+package org.example.greenshadowsystem.entity.impl;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.greenshadowsystem.entity.SuperEntity;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "fielded_staff")
+public class FieldedStaff implements SuperEntity {
+    @EmbeddedId
+    private FieldedStaffID fieldedStaffID;
+    private Date date;
+    @ManyToOne
+    @MapsId("staffId")
+    @JoinColumn(name = "staffId", referencedColumnName = "staffId")
+    private StaffEntity staff;
+    @ManyToOne
+    @MapsId("fieldCode")
+    @JoinColumn(name = "fieldCode", referencedColumnName = "fieldCode")
+    private FieldEntity field;
+}
