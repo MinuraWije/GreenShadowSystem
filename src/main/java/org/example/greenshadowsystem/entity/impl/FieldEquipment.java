aaca890b90b4a07a45131d0b906a8a6cae1901db
MinuraWije <minurawije@gmail.com>
2024-11-15 12:02:20 +0530

    created associate entity for field equipment

---
+++ src/main/java/org/example/greenshadowsystem/entity/impl/FieldEquipment.java
@@ -0,0 +1,27 @@
+package org.example.greenshadowsystem.entity.impl;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.greenshadowsystem.entity.SuperEntity;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "field_equipment")
+public class FieldEquipment implements SuperEntity {
+    @EmbeddedId
+    private FieldEquipmentID fieldEquipmentID;
+    @Enumerated(EnumType.STRING)
+    private Availability status;
+    @ManyToOne
+    @MapsId("equipmentId")
+    @JoinColumn(name = "equipmentId", referencedColumnName = "equipmentId")
+    private EquipmentEntity equipment;
+    @ManyToOne
+    @MapsId("fieldCode")
+    @JoinColumn(name = "fieldCode", referencedColumnName = "fieldCode")
+    private FieldEntity field;
+}
