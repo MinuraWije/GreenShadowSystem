48701b894c2a0bc36bf9e0c8e3e5e93446a89f80
MinuraWije <minurawije@gmail.com>
2024-11-15 12:01:29 +0530

    created embedded class for field equipment

---
+++ src/main/java/org/example/greenshadowsystem/entity/impl/FieldEquipmentID.java
@@ -0,0 +1,15 @@
+package org.example.greenshadowsystem.entity.impl;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Embeddable
+public class FieldEquipmentID {
+    private String fieldCode;
+    private String equipmentID;
+}
