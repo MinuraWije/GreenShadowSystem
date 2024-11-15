065b470f1b286ec85f8a5b5c850dfa4f6fade0b0
MinuraWije <minurawije@gmail.com>
2024-11-15 11:04:46 +0530

    created associate entity for staff equipments

---
+++ src/main/java/org/example/greenshadowsystem/entity/impl/StaffEquipment.java
@@ -0,0 +1,30 @@
+package org.example.greenshadowsystem.entity.impl;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.greenshadowsystem.entity.SuperEntity;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "staff_equipment")
+public class StaffEquipment implements SuperEntity {
+    @EmbeddedId
+    private StaffEquipmentID staffEquipmentID;
+    private Date date;
+    @Enumerated(EnumType.STRING)
+    private Availability status;
+    @ManyToOne
+    @MapsId("staffId")
+    @JoinColumn(name = "staffId", referencedColumnName = "staffId")
+    private StaffEntity staff;
+    @ManyToOne
+    @MapsId("equipmentId")
+    @JoinColumn(name = "equipmentId", referencedColumnName = "equipmentId")
+    private EquipmentEntity equipment;
+}
