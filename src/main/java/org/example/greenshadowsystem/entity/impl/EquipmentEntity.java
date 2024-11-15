75123498c8fb2e9175c91e132976ee39ee020cb1
MinuraWije <minurawije@gmail.com>
2024-11-15 11:54:28 +0530

    added all associate entity relations and annotations

--- src/main/java/org/example/greenshadowsystem/entity/impl/EquipmentEntity.java
+++ src/main/java/org/example/greenshadowsystem/entity/impl/EquipmentEntity.java
@@ -2,11 +2,14 @@ package org.example.greenshadowsystem.entity.impl;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.greenshadowsystem.entity.SuperEntity;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,4 +22,8 @@ public class EquipmentEntity implements SuperEntity {
     private String name;
     private String type;
     private Enum status;
+    @OneToMany(mappedBy = "equipment")
+    private List<FieldEquipment> fieldEquipments;
+    @OneToMany(mappedBy = "equipment")
+    private List<StaffEquipment> staffEquipments;
 }
