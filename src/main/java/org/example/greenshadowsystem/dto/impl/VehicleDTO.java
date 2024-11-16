c56725af14ff392f99d50dccfc36422d0ecdcffe
MinuraWije <minurawije@gmail.com>
2024-11-16 10:26:41 +0530

    created vehicleDTO

---
+++ src/main/java/org/example/greenshadowsystem/dto/impl/VehicleDTO.java
@@ -0,0 +1,21 @@
+package org.example.greenshadowsystem.dto.impl;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.greenshadowsystem.dto.VehicleStatus;
+import org.example.greenshadowsystem.entity.impl.Availability;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class VehicleDTO implements VehicleStatus {
+    private String vehicleCode;
+    private String licensePlateNum;
+    private String category;
+    private Double fuelType;
+    @Enumerated(EnumType.STRING)
+    private Availability status;
+}
