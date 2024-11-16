d246ab18e6b80ddc20d79a9cbdc15a25cfa94b2b
MinuraWije <minurawije@gmail.com>
2024-11-16 11:07:10 +0530

    created staff vehicle DTO

---
+++ src/main/java/org/example/greenshadowsystem/dto/impl/StaffVehicleDTO.java
@@ -0,0 +1,21 @@
+package org.example.greenshadowsystem.dto.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.greenshadowsystem.dto.StaffVehicleStatus;
+import org.example.greenshadowsystem.entity.impl.Availability;
+import org.example.greenshadowsystem.entity.impl.StaffVehicleID;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class StaffVehicleDTO implements StaffVehicleStatus {
+    private StaffVehicleID staffVehicleID;
+    private Date assignedDate;
+    private Availability status;
+    private StaffDTO staff;
+    private VehicleDTO vehicle;
+}
