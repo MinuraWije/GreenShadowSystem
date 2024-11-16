a456fd989e0915283ea8f7948f86fe64a1f6549b
MinuraWije <minurawije@gmail.com>
2024-11-16 11:28:14 +0530

    created equipment DTO

---
+++ src/main/java/org/example/greenshadowsystem/dto/impl/EquipmentDTO.java
@@ -0,0 +1,21 @@
+package org.example.greenshadowsystem.dto.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.greenshadowsystem.dto.EquipmentStatus;
+import org.example.greenshadowsystem.entity.impl.Availability;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class EquipmentDTO implements EquipmentStatus {
+    private String equipmentId;
+    private String name;
+    private String type;
+    private Availability status;
+    private List<FieldEquipmentDTO> fieldEquipments;
+    private List<StaffEquipmentDTO> staffEquipments;
+}
