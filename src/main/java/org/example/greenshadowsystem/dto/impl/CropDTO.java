97baa7c2c58ad915cabda264ba31072f478d6e8e
MinuraWije <minurawije@gmail.com>
2024-11-16 11:59:42 +0530

    created crop DTO

---
+++ src/main/java/org/example/greenshadowsystem/dto/impl/CropDTO.java
@@ -0,0 +1,22 @@
+package org.example.greenshadowsystem.dto.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.greenshadowsystem.dto.CropStatus;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CropDTO implements CropStatus {
+    private String cropCode;
+    private String cropName;
+    private String cropScientificName;
+    private String category;
+    private String img;
+    private String season;
+    private FieldDTO field;
+    private List<LogDTO> cropLogs;
+}
