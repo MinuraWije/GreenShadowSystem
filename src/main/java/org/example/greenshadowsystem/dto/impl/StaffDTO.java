03cd6d04c65256e56b3ada4e1482fbe71e012f10
MinuraWije <minurawije@gmail.com>
2024-11-16 11:38:09 +0530

    created staff DTO

---
+++ src/main/java/org/example/greenshadowsystem/dto/impl/StaffDTO.java
@@ -0,0 +1,31 @@
+package org.example.greenshadowsystem.dto.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.greenshadowsystem.dto.StaffStatus;
+import org.example.greenshadowsystem.entity.Role;
+import org.springframework.data.geo.Point;
+
+import java.util.Date;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class StaffDTO implements StaffStatus {
+    private String staffId;
+    private String name;
+    private Role role;
+    private String designation;
+    private Point gender;
+    private Date joinedDate;
+    private String email;
+    private Date dob;
+    private String address;
+    private String contactNum;
+    private List<FieldedStaffDTO> fieldedStaffs;
+    private List<StaffVehicleDTO> staffVehicles;
+    private List<StaffEquipmentDTO> staffEquipments;
+    private List<StaffLogDTO> staffLogs;
+}
