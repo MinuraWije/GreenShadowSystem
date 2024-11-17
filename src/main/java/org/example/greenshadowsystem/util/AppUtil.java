bfb9332cf8af8048501aca7af240e8ecaa06d415
MinuraWije <minurawije@gmail.com>
2024-11-17 19:42:38 +0530

    updated code for equipment

--- src/main/java/org/example/greenshadowsystem/util/AppUtil.java
+++ src/main/java/org/example/greenshadowsystem/util/AppUtil.java
@@ -7,10 +7,11 @@ public class AppUtil {
     public static String generateStaffId(){
         return "Staff-"+UUID.randomUUID();
     }
-
     public static String generateVehicleId(){
         return "Vehicle-"+UUID.randomUUID();
     }
+    public static String generateEquipmentId(){return "Equipment-"+UUID.randomUUID();}
+
     public static String profilePicToBase64(byte [] profilePic){
         return Base64.getEncoder().encodeToString(profilePic);
     }
