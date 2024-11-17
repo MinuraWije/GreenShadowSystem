3c160541b863e7a9913a47b1da344c31a1fa1277
MinuraWije <minurawije@gmail.com>
2024-11-17 20:06:13 +0530

    updated code to add equipment status

--- src/main/java/org/example/greenshadowsystem/customStatusCodes/SelectedCustomErrorStatus.java
+++ src/main/java/org/example/greenshadowsystem/customStatusCodes/SelectedCustomErrorStatus.java
@@ -3,13 +3,14 @@ package org.example.greenshadowsystem.customStatusCodes;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.greenshadowsystem.dto.EquipmentStatus;
 import org.example.greenshadowsystem.dto.StaffStatus;
 import org.example.greenshadowsystem.dto.VehicleStatus;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class SelectedCustomErrorStatus implements VehicleStatus, StaffStatus {
+public class SelectedCustomErrorStatus implements VehicleStatus, StaffStatus, EquipmentStatus {
     private int statusCode;
     private String statusMessage;
 }
