218c6a412240087409770ae5da614bacf5ba8938
MinuraWije <minurawije@gmail.com>
2024-11-17 19:24:41 +0530

    updated code to include Staff status

--- src/main/java/org/example/greenshadowsystem/customStatusCodes/SelectedCustomErrorStatus.java
+++ src/main/java/org/example/greenshadowsystem/customStatusCodes/SelectedCustomErrorStatus.java
@@ -3,12 +3,13 @@ package org.example.greenshadowsystem.customStatusCodes;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.greenshadowsystem.dto.StaffStatus;
 import org.example.greenshadowsystem.dto.VehicleStatus;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class SelectedCustomErrorStatus implements VehicleStatus{
+public class SelectedCustomErrorStatus implements VehicleStatus, StaffStatus {
     private int statusCode;
     private String statusMessage;
 }
