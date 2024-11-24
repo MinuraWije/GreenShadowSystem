39d62e7228c216078495bc282d5f32c0d021028f
MinuraWije <minurawije@gmail.com>
2024-11-24 11:22:31 +0530

    updated code for crop id generator

--- src/main/java/org/example/greenshadowsystem/util/AppUtil.java
+++ src/main/java/org/example/greenshadowsystem/util/AppUtil.java
@@ -13,7 +13,7 @@ public class AppUtil {
     public static String generateEquipmentId(){return "Equipment-"+UUID.randomUUID();}
     public static String generateFieldId(){return "Field-"+UUID.randomUUID();}
     public static String generateLogId(){return "Log-"+UUID.randomUUID();}
-
+    public static String generateCropId(){return "Crop-"+UUID.randomUUID();}
     public static String profilePicToBase64(byte [] profilePic){
         return Base64.getEncoder().encodeToString(profilePic);
     }
