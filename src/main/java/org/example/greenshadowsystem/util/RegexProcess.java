b79e238ec471c57058318939f61e5f4f2e083905
MinuraWije <minurawije@gmail.com>
2024-11-25 10:49:11 +0530

    updated regex checkers.

--- src/main/java/org/example/greenshadowsystem/util/RegexProcess.java
+++ src/main/java/org/example/greenshadowsystem/util/RegexProcess.java
@@ -4,32 +4,32 @@ import java.util.regex.Pattern;
 
 public class RegexProcess {
     public static boolean staffIdMatcher(String staffId) {
-        String regexForStaffId = "^STAFF-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        String regexForStaffId = "^Staff-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
         Pattern regexPattern = Pattern.compile(regexForStaffId);
         return regexPattern.matcher(staffId).matches();
     }
     public static boolean vehicleIdMatcher(String vehicleId){
-        String regexForVehicleId = "^VEHICLE-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        String regexForVehicleId = "^Vehicle-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
         Pattern regexPattern = Pattern.compile(regexForVehicleId);
         return regexPattern.matcher(vehicleId).matches();
     }
     public static boolean equipmentIdMatcher(String equipmentId){
-        String regexForEquipmentId = "^EQUIPMENT-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        String regexForEquipmentId = "^Equipment-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
         Pattern regexPattern = Pattern.compile(regexForEquipmentId);
         return regexPattern.matcher(equipmentId).matches();
     }
     public static boolean fieldIdMatcher(String fieldId){
-        String regexForFieldId = "^FIELD-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        String regexForFieldId = "^Field-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
         Pattern regexPattern = Pattern.compile(regexForFieldId);
         return regexPattern.matcher(fieldId).matches();
     }
     public static boolean logIdMatcher(String logId){
-        String regexForLogId = "^LOG-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        String regexForLogId = "^Log-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
         Pattern regexPattern = Pattern.compile(regexForLogId);
         return regexPattern.matcher(logId).matches();
     }
     public static boolean cropIdMatcher(String cropId){
-        String regexForCropId = "^CROP-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        String regexForCropId = "^Crop-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
         Pattern regexPattern = Pattern.compile(regexForCropId);
         return regexPattern.matcher(cropId).matches();
     }
