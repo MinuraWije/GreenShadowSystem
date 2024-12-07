d97eba32fe3332630ff4902b39d0b9999817a6be
MinuraWije <minurawije@gmail.com>
2024-12-07 07:14:35 +0530

    created user entity

---
+++ src/main/java/org/example/greenshadowsystem/entity/impl/UserEntity.java
@@ -0,0 +1,57 @@
+package org.example.greenshadowsystem.entity.impl;
+
+import org.example.greenshadowsystem.entity.Role;
+import org.example.greenshadowsystem.entity.SuperEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.greenshadowsystem.entity.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class UserEntity implements UserDetails,SuperEntity {
+    @Id
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+userRole.name()));
+        return authorities;
+    }
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
