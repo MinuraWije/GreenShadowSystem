dc62b86c686b5fa5f211d7ed1800739990ecf847
MinuraWije <minurawije@gmail.com>
2024-12-07 07:14:09 +0530

    created user dao

---
+++ src/main/java/org/example/greenshadowsystem/dao/UserDao.java
@@ -0,0 +1,13 @@
+package org.example.greenshadowsystem.dao;
+
+
+import org.example.greenshadowsystem.entity.impl.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDao extends JpaRepository<UserEntity,String> {
+    Optional<UserEntity> findByEmail(String email);
+}
