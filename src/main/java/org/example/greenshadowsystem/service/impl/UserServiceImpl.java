63a133de94165af9d1e5cd217ada9785557d62c5
MinuraWije <minurawije@gmail.com>
2024-12-07 07:10:22 +0530

    created user service impl for user

---
+++ src/main/java/org/example/greenshadowsystem/service/impl/UserServiceImpl.java
@@ -0,0 +1,81 @@
+package org.example.greenshadowsystem.service.impl;
+
+import org.example.greenshadowsystem.customStatusCodes.SelectedCustomErrorStatus;
+import org.example.greenshadowsystem.dao.UserDao;
+import org.example.greenshadowsystem.dto.UserStatus;
+import org.example.greenshadowsystem.dto.impl.UserDTO;
+import org.example.greenshadowsystem.entity.impl.UserEntity;
+import org.example.greenshadowsystem.exception.DataPersistException;
+import org.example.greenshadowsystem.exception.UserNotFoundException;
+import org.example.greenshadowsystem.service.UserService;
+import org.example.greenshadowsystem.util.Mapping;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private Mapping mapping;
+    @Override
+    public void saveuser(UserDTO userDTO) {
+        /*UserEntity savedUser = userDao.save(mapping.toUserEntity(userDTO));
+        mapping.touserDTO(savedUser);*/
+        UserEntity savedUser = userDao.save(mapping.toUserEntity(userDTO));
+        if(savedUser == null){
+            throw new DataPersistException("User not saved.");
+        }
+    }
+
+    @Override
+    public List<UserDTO> getAllUser() {
+        List<UserEntity> allUsers = userDao.findAll();
+        return mapping.asUserDTOList(allUsers);
+    }
+
+    @Override
+    public UserStatus getUser(String userId) {
+        if(userDao.existsById(userId)){
+            UserEntity selectedUser = userDao.getReferenceById(userId);
+            return mapping.touserDTO(selectedUser);
+        }else{
+            return new SelectedUserAndNoteErrorStatus(2, "User with id "+ userId + "not found.");
+        }
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+        Optional<UserEntity> existedUser = userDao.findById(userId);
+        if(!existedUser.isPresent()){
+            throw new UserNotFoundException("User with id "+userId+" not found");
+        }else {
+            userDao.deleteById(userId);
+        }
+    }
+
+    @Override
+    public void updateUser(String userID, UserDTO userDTO) {
+        Optional<UserEntity> tmpUser = userDao.findById(userID);
+        if(tmpUser.isPresent()){
+            tmpUser.get().setFirstName(userDTO.getFirstName());
+            tmpUser.get().setLastName(userDTO.getLastName());
+            tmpUser.get().setEmail(userDTO.getEmail());
+            tmpUser.get().setPassword(userDTO.getPassword());
+            tmpUser.get().setProfilePic(userDTO.getProfilePic());
+        }
+    }
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        return userName ->
+                userDao.findByEmail(userName)
+                        .orElseThrow(()->new UserNotFoundException("User not found"));
+    }
+}
